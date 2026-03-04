28540bd0e8b754aabbcbe551cdd5bb79ab38dec8
marquisdan <marquisdan@hotmail.com>
2026-03-04 15:36:07 -0600

    Initial commit

---
+++ TepBest/Models/SortColumns.cs
@@ -0,0 +1,18 @@
+namespace TepBest.Models;
+
+public static class SortColumns
+{
+    public const string AvgDmg     = "Average Total Damage";
+    public const string IfAllHit   = "If All Hit";
+    public const string HitChance  = "Hit Chance";
+    public const string AttackDice = "Attack Dice";
+    public const string DamageDice = "Damage Dice";
+    public const string NumAttacks = "Number of Attacks";
+    public const string None       = "None";
+
+    public static readonly string[] Sort1Options =
+        [AvgDmg, HitChance, IfAllHit, AttackDice, DamageDice, NumAttacks];
+
+    public static readonly string[] Sort2Options =
+        [None, AvgDmg, HitChance, IfAllHit,  AttackDice, DamageDice, NumAttacks];
+}
